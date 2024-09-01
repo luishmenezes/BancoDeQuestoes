842f906dea2fb55ba3346693fc57438c08bf4077
David Pontes <pondavid103@gmail.com>
2024-09-01 11:42:02 -0300

    feat:models Questao

--- src/main/java/com/example/BancoDeDados/Services/ServiceTratarTexto.java
+++ src/main/java/com/example/BancoDeDados/Services/ServiceTratarTexto.java
@@ -1,5 +1,7 @@
 package com.example.BancoDeDados.Services;
 
+import com.example.BancoDeDados.Model.Questao;
+
 import java.io.IOException;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -7,6 +9,7 @@ import java.util.regex.Pattern;
 public class ServiceTratarTexto {
 
     private ServicePDF texto = new ServicePDF();
+    private Questao questao=new Questao();
 
     public String pegarQuestao() throws IOException {
         StringBuilder resultado = new StringBuilder();
@@ -20,6 +23,20 @@ public class ServiceTratarTexto {
 
         return resultado.toString();
     }
+    public String pegarCabecalho() throws IOException {
+        StringBuilder resultado = new StringBuilder();
+
+        Pattern ModeloCabecalho = Pattern.compile("(\\d+\\.\\s\\([^\\)]+\\))", Pattern.DOTALL);
+        Matcher Cabecalho = ModeloCabecalho.matcher(texto.TextoExtraido());
+
+        while (Cabecalho.find()) {
+            resultado.append(Cabecalho.group()).append("\n");
+        }
+        questao.setCabecalho(resultado.toString());
+        return resultado.toString();
+    }
+
+
     public String mandartextoFiltrado() throws IOException {
         return pegarQuestao();
     }
