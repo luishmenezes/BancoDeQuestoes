3db05ece76d5681f9e59a95603cccef3cdae148c
David Pontes <pondavid103@gmail.com>
2024-10-02 23:26:45 -0300

    add: jakarta in model user

--- src/main/java/com/example/BancoDeDados/Model/Usuario.java
+++ src/main/java/com/example/BancoDeDados/Model/Usuario.java
@@ -1,5 +1,6 @@
 package com.example.BancoDeDados.Model;
 
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -7,14 +8,27 @@ import org.springframework.format.annotation.DateTimeFormat;
 
 import java.util.Date;
 
+@Entity
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
+
 public class Usuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
+
+    @Column(nullable = false)
     private String nome;
+
+    @Column(unique = true, nullable = false)
     private String email;
+
+    @Column(nullable = false)
     private String senha;
+
+    @Column(nullable = false)
     @DateTimeFormat(pattern = "yyyy-MM-dd")
     private Date dataNascimento;
 }
