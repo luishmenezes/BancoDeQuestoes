b9dc3f20692e7c1f5095ad879e2b61f6003c9a6a
David Pontes <pondavid103@gmail.com>
2024-10-05 18:33:40 -0300

    add:model Estudante

---
+++ src/main/java/com/example/BancoDeDados/Model/Escola.java
@@ -0,0 +1,37 @@
+package com.example.BancoDeDados.Model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.util.Date;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@EqualsAndHashCode(of="id")
+@Entity(name="escola")
+@Table(name="escola")
+public class Escola {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(nullable = false)
+    private String nome;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String senha;
+
+    @Column(nullable = false)
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    private Date dataNascimento;
+
+}
